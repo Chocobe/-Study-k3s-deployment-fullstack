3ead684813d72ac17e0709dcbf653437987613eb
Miles <kyw05171@gmail.com>
2025-06-08 19:10:27 +0900

    Install typescript to backend

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/api/hello', (_req, res) => {
+  res.json('Hello from Typescript backend');
+});
+
+app.listen(PORT, error => {
+  if (error) {
+    console.group('Failed to start backend server');
+    console.error(error);
+    console.groupEnd();
+
+    return;
+  }
+
+  console.group('--- --- --- --- --- --- --- --- --- --- ');
+  console.log(`Server is running at http://localhost:${PORT}`);
+  console.group('--- --- --- --- --- --- --- --- --- --- ');
+});
